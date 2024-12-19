00692f22c551f87030a86a57cf3174c0d2e878a4
David <ingamesy@gmail.com>
2024-12-19 19:00:53 +0500

    ToDoList_part_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from "react";
-import LoginForm from "./components/Loginform.jsx";
+import React, { useState } from "react";
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([...tasks, newTask]);
+      setNewTask("");
+    }
+  };
   return (
-    <div>
-      <LoginForm />
+    <div className="app">
+      <div className="cont">
+        <h1>ToDo list</h1>
+        <input
+          type="text"
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="write your tasks"
+          value={newTask}
+        />
+        <button onClick={addTask}>add new task</button>
+        <ol>
+          {tasks.map((task, index) => {
+            return <li key={index}>{task}</li>;
+          })}
+        </ol>
+      </div>
     </div>
   );
 }
